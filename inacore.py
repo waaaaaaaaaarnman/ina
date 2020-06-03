34c457188ff99d7b928a014d95b8df4071c5ce46
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-03 14:15:03 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -1,7 +1,8 @@
 import discord
-from googlesearch import search
+from googlesearch import search 
+
 client = discord.Client()
-# 起動時のメッセージ
+
 @client.event
 async def on_ready():
  open = client.get_channel(717604364587630693)
@@ -22,4 +23,5 @@ async def on_message(message):
     break
  if message.content.startwith('こん'):
   await message.channel.send('こんにちは！')
+
 client.run('NzE3NTkwOTc2MTU1MjIyMDI4.XtcmVw.k8QN9W-LtTZyhNc1J2A6mi89aMs')
