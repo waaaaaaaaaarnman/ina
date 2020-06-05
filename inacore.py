862aee839253a67a8e930aaf99c2ecaa3a04cc4d
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-05 20:36:27 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -1,25 +1,21 @@
-import discord
-from googlesearch import search 
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
- open = client.get_channel(717604364587630693)
- await open.send('opened!')
-@client.event
-async def on_message(message):
- if message.author.bot:
-  return
- if message.content == 'i!kensaku':
-  tango = message.content
-  kensaku = tango.split(' ')[1]
-  count = 0
-  await message.channel.send('以下のものが見つかったよ。')
-  for url in search(kensaku, lang="jp",num = 5):
-   await message.channel.send(url)
-   count += 1
-   if(count == 5):
-    break
-
-client.run('NzE3NTkwOTc2MTU1MjIyMDI4.XtcmVw.k8QN9W-LtTZyhNc1J2A6mi89aMs')
+from discord.ext import commands
+import traceback
+INITIAL_EXTENSIONS = [
+    'cogs.inacog'
+]
+class inachancog(commands.Bot):
+    def __init__(self, command_prefix):
+        super().__init__(command_prefix)
+        for cog in INITIAL_EXTENSIONS:
+            try:
+                self.load_extension(cog)
+            except Exception:
+                traceback.print_exc()
+    async def on_ready(self):
+        print('-----')
+        print(self.user.name)
+        print(self.user.id)
+        print('-----')
+if __name__ == '__main__':
+    bot = MyBot(command_prefix='i!')
+    bot.run('OU02s4VtcIYcI8wHBeIzkwn4R30aXNLX')
