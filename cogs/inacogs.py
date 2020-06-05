eb3d2573f4308782ebc924f785f5b10e76c94888
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-05 20:38:13 +0900

    Create inacogs.py

---
+++ cogs/inacogs.py
@@ -0,0 +1,9 @@
+from discord.ext import commands
+class inacog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('pong!')
+def setup(bot):
+    bot.add_cog(inacog(bot)) # TestCogにBotを渡してインスタンス化し、Botにコグとして登録する。
