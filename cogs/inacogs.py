d6e741282f0e52c653cf990ed701ca8c5988bece
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-05 20:45:47 +0900

    Delete inacogs.py

--- cogs/inacogs.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands
-class inacog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send('pong!')
-def setup(bot):
-    bot.add_cog(inacog(bot))
