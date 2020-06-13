a3d5443527af7a50d7cd2ccec717aa77c9da36a6
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-13 23:00:40 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -1,14 +1,11 @@
 from discord.ext import commands
 import traceback
+
 bot = commands.Bot(command_prefix='i!')
+
 @bot.event
 async def on_command_error(ctx, error):
-    ch = 721361976122540053
-    embed = discord.Embed(title="エラー情報", description="", color=0xf00)
-    embed.add_field(name="エラー発生コマンド", value=ctx.message.content, inline=False)
-    embed.add_field(name="発生エラー", value=error, inline=False)
-    m = await bot.get_channel(ch).send(embed=embed)
-    await ctx.send("{m.id}")
+ print(error,ctx.message.content)
 
 @bot.command()
 async def ping(ctx):
