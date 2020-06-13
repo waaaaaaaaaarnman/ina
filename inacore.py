d2d3a946c5b2de1796454fa603e3e23212f63b42
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-13 23:30:24 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import traceback
 
 bot = commands.Bot(command_prefix='i!')
-@bot.event()
+@bot.event
 async def on_command_error(ctx, error):
     ch = 721361976122540053
     embed = discord.Embed(title="エラー情報", description="", color=0xf00)
@@ -14,7 +14,7 @@ async def on_command_error(ctx, error):
     embed.add_field(name="発生エラー", value=error, inline=False)
     m = await bot.get_channel(ch).send(embed=embed)
     await ctx.send(f"何らかのエラーが発生しました。ごめんなさい。\nこのエラーについて問い合わせるときはこのコードも一緒にお知らせください：{m.id}")
-@bot.event()
+@bot.event
 async def on_ready(self):
         print('-----')
         print(self.user.name)
