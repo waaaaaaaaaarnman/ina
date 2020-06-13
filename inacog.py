10dc4ae95d0266bb983d1448e4aa0371ec2893ea
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-13 14:52:13 +0900

    Create inacog.py

---
+++ inacog.py
@@ -0,0 +1,9 @@
+from discord.ext import commands
+class TestCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('pong!')
+def setup(bot):
+    bot.add_cog(TestCog(bot))
