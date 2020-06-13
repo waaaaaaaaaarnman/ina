98a1526df6045aaf58f91b518c93a9ad7954116d
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-13 22:58:02 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -5,10 +5,6 @@ bot = commands.Bot(command_prefix='i!')
 async def on_command_error(ctx, error):
     ch = 721361976122540053
     embed = discord.Embed(title="エラー情報", description="", color=0xf00)
-    embed.add_field(name="エラー発生サーバー名", value=ctx.guild.name, inline=False)
-    embed.add_field(name="エラー発生サーバーID", value=ctx.guild.id, inline=False)
-    embed.add_field(name="エラー発生ユーザー名", value=ctx.author.name, inline=False)
-    embed.add_field(name="エラー発生ユーザーID", value=ctx.author.id, inline=False)
     embed.add_field(name="エラー発生コマンド", value=ctx.message.content, inline=False)
     embed.add_field(name="発生エラー", value=error, inline=False)
     m = await bot.get_channel(ch).send(embed=embed)
