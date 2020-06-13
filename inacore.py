04b8658dbbe6c0e5ac56c226c3e43586e9f1e7fa
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-13 22:46:44 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -1,16 +1,18 @@
 from discord.ext import commands
-import os
 import traceback
-
-bot = commands.Bot(command_prefix='/')
-
-
+bot = commands.Bot(command_prefix='i!')
 @bot.event
+@commands.dm_only()
 async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
+    embed = discord.Embed(title="エラー情報", description="", color=0xf00)
+    embed.add_field(name="エラー発生サーバー名", value=ctx.guild.name, inline=False)
+    embed.add_field(name="エラー発生サーバーID", value=ctx.guild.id, inline=False)
+    embed.add_field(name="エラー発生ユーザー名", value=ctx.author.name, inline=False)
+    embed.add_field(name="エラー発生ユーザーID", value=ctx.author.id, inline=False)
+    embed.add_field(name="エラー発生コマンド", value=ctx.message.content, inline=False)
+    embed.add_field(name="発生エラー", value=error, inline=False)
+    await ctx.send(f"何らかのエラーが発生しました。ごめんなさい。")
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def ping(ctx):
