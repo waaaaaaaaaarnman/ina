174f76c48c69750a125ed712c492a28e57ada0cd
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-14 18:13:21 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 import discord
 import cogs.inacog as inacog
-
+import cogs.inacog2 as inacog2
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='i!')
@@ -13,5 +13,6 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    inacog.setup(bot)
     inacog.setup(bot)
 bot.run('NzE3NTkwOTc2MTU1MjIyMDI4.XuRqJQ.sesJjpbvS2ojf9fiJ-FJ0kFZbpk')
