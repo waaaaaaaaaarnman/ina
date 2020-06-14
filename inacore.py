35e5ae47eec443d0af6c7fa676f432cc289252da
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-14 16:27:25 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -5,17 +5,8 @@ from googlesearch import search
 from googletrans import Translator
 bot = commands.Bot(command_prefix='i!')
 @bot.event
-async def on_command_error(ctx, error):
-    ch = 721361976122540053
-    embed = discord.Embed(title="エラー情報", description="", color=0xf00)
-    embed.add_field(name="エラー発生サーバー名", value=ctx.guild.name, inline=False)
-    embed.add_field(name="エラー発生サーバーID", value=ctx.guild.id, inline=False)
-    embed.add_field(name="エラー発生ユーザー名", value=ctx.author.name, inline=False)
-    embed.add_field(name="エラー発生ユーザーID", value=ctx.author.id, inline=False)
-    embed.add_field(name="エラー発生コマンド", value=ctx.message.content, inline=False)
-    embed.add_field(name="発生エラー", value=error, inline=False)
-    m = await bot.get_channel(ch).send(embed=embed)
-    await ctx.send(f"何らかのエラーが発生しました。ごめんなさい。\nこのエラーについて問い合わせるときはこのコードも一緒にお知らせください：{m.id}")
+async def on_command_error(self,ctx, error):
+ ctx.send('エラー',error)
 @bot.event
 async def on_ready():
  await bot.get_channel(717604364587630693).send('opened!')
