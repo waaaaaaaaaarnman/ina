d7ed0aa12133be8677c3ba2440f893ed7400b361
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-14 18:11:31 +0900

    Update inacog.py

--- cogs/inacog.py
+++ cogs/inacog.py
@@ -3,6 +3,11 @@ from discord.ext import commands
 class inacog(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
-        await ctx.send('pong!')
+     await ctx.send('pong!')
+    @commands.command()
+    async def what(self, ctx, what):
+     await ctx.send(f'{what}とはなんですか？')
 def setup(bot):
     bot.add_cog(inacog(bot))
+
+    
