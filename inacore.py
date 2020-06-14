059ca1a8eaba9ce42176feff9175ec5347dce800
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-14 17:52:50 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -1,9 +1,6 @@
 #coding: utf-8
-import os
-import configparser
 import discord
-import random
-import cogs.game as game
+import cogs.inacog as inacog
 
 from discord.ext import commands
 
@@ -16,43 +13,5 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-
+    game.setup(bot)
 bot.run('NzE3NTkwOTc2MTU1MjIyMDI4.XuRqJQ.sesJjpbvS2ojf9fiJ-FJ0kFZbpk')
-
-import discord
-from discord.ext import commands
-import traceback
-from googlesearch import search
-from googletrans import Translator
-bot = commands.Bot(command_prefix='i!')
-client = discord.client()
-@bot.event
-async def on_command_error(self,ctx, error):
- ctx.send('エラー',error)
-@bot.event
-async def on_ready():
- await bot.get_channel(717604364587630693).send('opened!')
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-async def search(ctx):
-    count = 0
-    for url in search(ctx.message.content, lang="jp",num = 5):
-     ctx.send(url)
-     count += 1
-     if(count == 5):
-      break
-async def honyaku(ctx):
- translator = Translator()
- aaa = translator.translate(ctx.message.content, src='en', dest='ja')
- await ctx.send(aaa.text)
-@client.event
-async def on_message(message):
- if message.content == 'aaa':
-  await message.channel.send('aa')
-             
-    
-
-bot.run('NzE3NTkwOTc2MTU1MjIyMDI4.XuRqJQ.sesJjpbvS2ojf9fiJ-FJ0kFZbpk')
-client.run('NzE3NTkwOTc2MTU1MjIyMDI4.XuRqJQ.sesJjpbvS2ojf9fiJ-FJ0kFZbpk')
