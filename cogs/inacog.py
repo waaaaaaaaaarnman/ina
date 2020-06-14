02435845222c8798cbda019faf783718a6052d34
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-14 18:21:50 +0900

    Update inacog.py

--- cogs/inacog.py
+++ cogs/inacog.py
@@ -7,6 +7,12 @@ class inacog(commands.Cog):
     @commands.command()
     async def what(self, ctx, what):
      await ctx.send(f'{what}とはなんですか？')
+    @commands.Cog.listener()
+    async def on_message(self, message):
+     if message.author.bot:
+            return
+     if message.content == 'hello':
+            await message.channel.send('hello')
 def setup(bot):
     bot.add_cog(inacog(bot))
 
