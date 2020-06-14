f916d5720ef39f43d4d862a76e7e83d8a9ec9148
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-14 22:01:40 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -2,6 +2,7 @@
 import discord
 import cogs.inacog as inacog
 import cogs.inacog2 as inacog2
+import cogs.yomiage as yomiage
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='i!')
@@ -18,4 +19,5 @@ async def on_ready():
     print('------')
     inacog.setup(bot)
     inacog2.setup(bot)
+    yomiage.setup(bot)
 bot.run('NzE3NTkwOTc2MTU1MjIyMDI4.XuRqJQ.sesJjpbvS2ojf9fiJ-FJ0kFZbpk')
