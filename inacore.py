0364348b675e55bf7b0dde71c2511a1d16b2e8fa
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-14 22:16:09 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -7,9 +7,6 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='i!')
 
-if not discord.opus.is_loaded():
-    discord.opus.load_opus("heroku-buildpack-libopus")
-
 @bot.event
 async def on_ready():
     print('------')
