30bafb16e1c59b0112db88f082340be28554d439
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-21 21:33:23 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -3,19 +3,9 @@ import discord
 import cogs.inacog as inacog
 import cogs.inacog2 as inacog2
 from discord.ext import commands
-import time
 bot = commands.Bot(command_prefix='i!',command=None)
 bot.remove_command("help")
-def test():
- while True:
-     await bot.change_presence(activity=discord.Game("."))
-     time.sleep(0.5)
-     await bot.change_presence(activity=discord.Game(".."))
-     time.sleep(0.5)
-     await bot.change_presence(activity=discord.Game("..."))
-     time.sleep(0.5)
-     await bot.change_presence(activity=discord.Game("...."))
-     time.sleep(0.5)
+
 @bot.event
 async def on_command_error(ctx, error):
  await ctx.send(f'{error}が発生しました')
@@ -29,6 +19,6 @@ async def on_ready():
     print('------')
     inacog.setup(bot)
     inacog2.setup(bot)
-    test()
+    await bot.change_presence(activity=discord.Game("test"))
     
 bot.run('NzE3NTkwOTc2MTU1MjIyMDI4.Xu2_bw.MD4oJ_ZuFNYljkLsJiZi3zxMTy4')
