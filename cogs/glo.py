accf2cc4d3c06c476c0d10a567adb30787290443
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-22 14:48:54 +0900

    Update glo.py

--- cogs/glo.py
+++ cogs/glo.py
@@ -6,7 +6,6 @@ class glo(commands.Cog):
  @commands.Cog.listener()
  async def on_message(ctx,message):
   if message.channel.id in global_channels:
-   await message.delete()
    for channel in global_channels:
     ch_webhooks = await channel.webhooks()
     webhook = discord.utils.get(ch_webhooks, name='test_webhook')
