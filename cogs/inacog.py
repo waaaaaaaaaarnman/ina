647826103697f7961bf9575c4a3324d2a2c74123
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-22 14:02:47 +0900

    Update inacog.py

--- cogs/inacog.py
+++ cogs/inacog.py
@@ -1,7 +1,5 @@
 from discord.ext import commands
 
-global_channels = [721168485681463350,724477146885521490]
-
 class inacog(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
@@ -22,16 +20,6 @@ class inacog(commands.Cog):
     @commands.command()
     async def test(ctx):
      ctx.send(ctx.channel.id)
-    @commands.Cog.listener()
-    async def on_message(message):
-     if message.channel.id in global_channels:
-      await message.delete()
-      for channel in global_channels:
-       ch_webhooks = await channel.webhooks()
-       webhook = discord.utils.get(ch_webhooks, name='test_webhook')
-      if webhook is None:
-       continue
-      await webhook.send(content=message.content,username=message.author.name,avatar_url=message.author.avatar_url_as(format="png")
                          
 def setup(bot):
     bot.add_cog(inacog(bot))
