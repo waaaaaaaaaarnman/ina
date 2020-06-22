a2ac5ed9f49fbb2ec800a7441bbdfc4af30137b3
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-22 14:53:20 +0900

    Update glo.py

--- cogs/glo.py
+++ cogs/glo.py
@@ -1,14 +1,13 @@
 from discord.ext import commands
 
-global_channels = ['721168485681463350','724477146885521490']
+global_channels = [721168485681463350,724477146885521490]
 
 class glo(commands.Cog):
  @commands.Cog.listener()
  async def on_message(ctx,message):
   if message.channel.id in global_channels:
    for channel in global_channels:
-    ch_webhooks = await channel.webhooks()
-    webhook = discord.utils.get(ch_webhooks, name='test_webhook')
+    webhook = discord.utils.get(await channel.webhooks(), name='test_webhook')
     if webhook is None:
      continue
     await webhook.send(content=message.content,
