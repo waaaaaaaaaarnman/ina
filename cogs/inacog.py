da2fa50d303cbbdbeed175a2ccda605f4f752b74
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-22 13:51:16 +0900

    Update inacog.py

--- cogs/inacog.py
+++ cogs/inacog.py
@@ -32,6 +32,7 @@ class inacog(commands.Cog):
       if webhook is None:
        continue
       await webhook.send(content=message.content,username=message.author.name,avatar_url=message.author.avatar_url_as(format="png")
+                         
 def setup(bot):
  bot.add_cog(inacog(bot))
 
