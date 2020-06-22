39d89cbadb3858800ac36f0136fac27bf00013c7
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-22 14:10:55 +0900

    Update glo.py

--- cogs/glo.py
+++ cogs/glo.py
@@ -3,16 +3,9 @@ from discord.ext import commands
 global_channels = [721168485681463350,724477146885521490]
 
 class glo(commands.Cog):
- @commands.Cog.listener()
- async def on_message(message):
-  if message.channel.id in global_channels:
-   await message.delete()
-   for channel in global_channels:
-    ch_webhooks = await channel.webhooks()
-    webhook = discord.utils.get(ch_webhooks, name='test_webhook')
-    if webhook is None:
-     continue
-    await webhook.send(content=message.content,username=message.author.name,avatar_url=message.author.avatar_url_as(format="png")
+ @commands.command
+ async def teest(ctx):
+  ctx.send('teest')
                          
 def setup(bot):
  bot.add_cog(glo(bot))
