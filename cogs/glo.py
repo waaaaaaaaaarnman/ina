4af001779fb345b7ed1b94e0a91afe161a46ff94
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-22 14:50:21 +0900

    Update glo.py

--- cogs/glo.py
+++ cogs/glo.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 
-global_channels = [721168485681463350,724477146885521490]
+global_channels = ['721168485681463350','724477146885521490']
 
 class glo(commands.Cog):
  @commands.Cog.listener()
