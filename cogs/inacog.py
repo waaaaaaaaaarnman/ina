7d8115a1f3f9ff09c33ec88faec0747a0ce31b59
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-22 13:37:00 +0900

    Update inacog.py

--- cogs/inacog.py
+++ cogs/inacog.py
@@ -28,14 +28,14 @@ class inacog(commands.Cog):
             return
      if message.content == 'hello':
             await message.channel.send('hello')
-    for message.channel.id == global_channels:
-     await message.delete()
-     for channel in global_channels:
-      ch_webhooks = await channel.webhooks()
-      webhook = discord.utils.get(ch_webhooks, name='test_webhook')
-     if webhook is None:
-      continue
-     await webhook.send(content=message.content,username=message.author.name,avatar_url=message.author.avatar_url_as(format="png")
+     for message.channel.id == global_channels:
+      await message.delete()
+      for channel in global_channels:
+       ch_webhooks = await channel.webhooks()
+       webhook = discord.utils.get(ch_webhooks, name='test_webhook')
+      if webhook is None:
+       continue
+      await webhook.send(content=message.content,username=message.author.name,avatar_url=message.author.avatar_url_as(format="png")
 
 def setup(bot):
     bot.add_cog(inacog(bot))
