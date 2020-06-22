d4827b09acc9ba04e70784e055df43a8398d9009
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-22 13:13:50 +0900

    Update inacog.py

--- cogs/inacog.py
+++ cogs/inacog.py
@@ -29,13 +29,15 @@ class inacog(commands.Cog):
             return
      if message.content == 'hello':
             await message.channel.send('hello')
-     global voich
-     # 接続
-     if message.content.startswith('/connect'):
-      voich = await VoiceChannel.connect(message.author.voice.channel)
-     # 切断
-     if message.content.startswith('/discon'):
-      await voich.disconnect()
+    await message.delete()
+    global_channels = [721168485681463350,724477146885521490]
+    for channel in global_channels:
+    ch_webhooks = await channel.webhooks()
+    webhook = discord.utils.get(ch_webhooks, name='test_webhook')
+    if webhook is None:
+      continue
+    await webhook.send(content=message.content,username=message.author.name,avatar_url=message.author.avatar_url_as(format="png")
+     
 def setup(bot):
     bot.add_cog(inacog(bot))
 
