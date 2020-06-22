e9e830df44df7c76f49bb135bb3189241bc17e5e
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-22 14:06:39 +0900

    Update glo.py

--- cogs/glo.py
+++ cogs/glo.py
@@ -11,7 +11,7 @@ class glo(commands.Cog):
     webhook = discord.utils.get(ch_webhooks, name='test_webhook')
     if webhook is None:
      continue
-   await webhook.send(content=message.content,username=message.author.name,avatar_url=message.author.avatar_url_as(format="png")
+    await webhook.send(content=message.content,username=message.author.name,avatar_url=message.author.avatar_url_as(format="png")
                          
 def setup(bot):
     bot.add_cog(glo(bot))
