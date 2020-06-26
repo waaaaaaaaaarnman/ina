2b20ad37bc28a19760c65f6bcb10c7a9c8bf4c6c
Kaede728 <65491942+Kaede728@users.noreply.github.com>
2020-06-27 08:49:33 +0900

    Update glo.py

--- cogs/glo.py
+++ cogs/glo.py
@@ -7,13 +7,8 @@ class glo(commands.Cog):
  async def on_message(ctx,message):
   if message.channel.id in global_channels:
    for channel in global_channels:
-    webhook = discord.utils.get(await channel.webhooks(), name='test_webhook')
-    if webhook is None:
-     continue
-    await webhook.send(content=message.content,
-    username=message.author.name,
-    avatar_url=message.author.avatar_url_as(format="png"))
-    await bot.process_commands(message)
+    messagech = channel
+    
 
 def setup(bot):
  bot.add_cog(glo(bot))
