21f6b72058a43e28da9974e7a55727201fdbc89d
waaaaaaaaaarnman <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-07-06 20:08:43 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -24,13 +24,6 @@ async def on_ready():
     inacog2.setup(bot)
     glo.setup(bot)
 
-@tasks.loop(seconds=3)
-async def loop():
- await bot.change_presence(activity=discord.Game("tes1"))
- await bot.change_presence(activity=discord.Game("tes2"))
- await bot.change_presence(activity=discord.Game("tes3"))
- await bot.change_presence(activity=discord.Game("tes4"))
 
-loop.start()
     
 bot.run('NzE3NTkwOTc2MTU1MjIyMDI4.Xu2_bw.MD4oJ_ZuFNYljkLsJiZi3zxMTy4')
