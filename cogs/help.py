dc8b85ef51b59388f4d5720b1fa44916d3347bf3
waaaaaaaaaarnman <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-07-06 20:19:20 +0900

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -1,10 +1,10 @@
 from discord.ext import commands
-import .data
+import data.data as data_file
 
 
 class help(commands.Cog):
  async def help(ctx,page):
-  send_page = .data(page)
+  send_page = data_file.get_page(page)
   if msg == None:
    msg = await ctx.send(send_page)
   else:
