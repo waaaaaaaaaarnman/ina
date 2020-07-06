c0ff4ec5a66c306cf32808371f4115b505ac8689
waaaaaaaaaarnman <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-07-06 20:10:55 +0900

    Create help.py

---
+++ cogs/help.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+
+
+class help(commands.Cog):
+ @tasks.loop(seconds=3)
+ async def loop():
+  await bot.change_presence(activity=discord.Game("tes1"))
+  await bot.change_presence(activity=discord.Game("tes2"))
+  await bot.change_presence(activity=discord.Game("tes3"))
+  await bot.change_presence(activity=discord.Game("tes4"))
+  
+   
+
+def setup(bot):
+ bot.add_cog(helq(bot))
