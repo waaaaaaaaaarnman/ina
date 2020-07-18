d96f0353b57d3368d254e9db1846025fa7d28361
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-07-18 16:36:43 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -4,10 +4,6 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='i!')
 
-@bot.command()
-async def ping(ctx):
- await ctx.send(f'pong!{bot.latency}')
-
 @bot.event
 async def on_ready():
     print('------')
@@ -15,24 +11,9 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-     return
-    GLOBAL_CH_NAME = "伊奈グローバル" 
-    GLOBAL_WEBHOOK_NAME = "ina-global-webhook"
-    if message.channel.name == GLOBAL_CH_NAME:
-        await message.delete()
-        channels = bot.get_all_channels()
-        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
-        for channel in global_channels:
-            ch_webhooks = await channel.webhooks()
-            webhook = discord.utils.get(ch_webhooks, name=GLOBAL_WEBHOOK_NAME)
-            if webhook is None:
-                await channel.create_webhook(name=GLOBAL_WEBHOOK_NAME)
-            await webhook.send(content=message.content,
-                username=message.author.name,
-                avatar_url=message.author.avatar_url_as(format="png"))
+    
+@bot.command()
+async def ping(ctx):
+ await ctx.send(f'pong!{bot.latency}')
 
 bot.run('NzE3NTkwOTc2MTU1MjIyMDI4.Xu2_bw.MD4oJ_ZuFNYljkLsJiZi3zxMTy4')
