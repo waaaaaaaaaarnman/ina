ad952b6f255ca2ca6efe5955e1cd49e63e9b84f1
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-07-18 11:20:13 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -21,6 +21,31 @@ async def on_ready():
     for b in a:
      print(b.split())
     a.close()
+@bot.event
+async def on_message(ctx):
+    message = ctx.message
+    if message.author.bot:
+        return
+    GLOBAL_CH_NAME = "hoge-global" # グローバルチャットのチャンネル名
+    GLOBAL_WEBHOOK_NAME = "hoge-webhook" # グローバルチャットのWebhook名
+
+    if message.channel.name == GLOBAL_CH_NAME:
+        # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
+        await message.delete()
+
+        channels = client.get_all_channels()
+        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+
+        for channel in global_channels:
+            ch_webhooks = await channel.webhooks()
+            webhook = discord.utils.get(ch_webhooks, name=GLOBAL_WEBHOOK_NAME)
+
+            if webhook is None:
+                # そのチャンネルに hoge-webhook というWebhookは無かったので無視
+                continue
+            await webhook.send(content=message.content,
+                username=message.author.name,
+                avatar_url=message.author.avatar_url_as(format="png"))
     
 @bot.command()
 async def test(ctx,test):
