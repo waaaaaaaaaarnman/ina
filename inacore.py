08aeec09a7da98634d64fd75141105775740e97b
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-07-18 16:54:07 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -1,25 +1,12 @@
 #coding: utf-8
 import discord
 from discord.ext import commands
-from discord.ext import tasks
-import time
 
 bot = commands.Bot(command_prefix='i!')
 
 @bot.command()
 async def ping(ctx):
  await ctx.send(f'pong!{bot.latency}')
- 
-@tasks.loop(seconds=40)
-async def loop():
- await bot.change_presence(activity=discord.Game("てすと"))
- time.sleep(10)
- await bot.change_presence(activity=discord.Game("てすとと"))
- time.sleep(10)
- await bot.change_presence(activity=discord.Game("てすととと"))
- time.sleep(10)
- await bot.change_presence(activity=discord.Game("てすとととと"))
- time.sleep(10)
 
 @bot.event
 async def on_ready():
@@ -28,6 +15,6 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    loop.start()
+    await bot.change_presence(activity=discord.Game("てすと"))
 
 bot.run('NzE3NTkwOTc2MTU1MjIyMDI4.Xu2_bw.MD4oJ_ZuFNYljkLsJiZi3zxMTy4')
