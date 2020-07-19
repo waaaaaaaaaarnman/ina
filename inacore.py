8d749f9250279a8c66c02f38875c0f8200b5fb70
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-07-19 14:45:03 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -3,9 +3,25 @@
 import discord
 from discord.ext import commands
 import cogs.inacog as inacog
+from discord.ext import tasks
+import time
 
 bot = commands.Bot(command_prefix='i!')
- 
+
+@bot.command()
+    async def test(ctx):
+     ctx.send(ctx.channel.id)
+
+@tasks.loop(seconds=40)
+async def loop():
+ await bot.change_presence(activity=discord.Game("てすと"))
+ time.sleep(10)
+ await bot.change_presence(activity=discord.Game("てすと"))
+ time.sleep(10)
+ await bot.change_presence(activity=discord.Game("てすと"))
+ time.sleep(10)
+ await bot.change_presence(activity=discord.Game("てすと"))
+ time.sleep(10)
 #機能時のログ
 @bot.event
 async def on_ready():
@@ -15,6 +31,5 @@ async def on_ready():
     print(bot.user.id)
     print('------')
     inacog.setup(bot)
-    await bot.change_presence(activity=discord.Game("てすと"))
 
 bot.run('NzE3NTkwOTc2MTU1MjIyMDI4.Xu2_bw.MD4oJ_ZuFNYljkLsJiZi3zxMTy4')
