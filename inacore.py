f287f5c19747f6da42f58e548445881875bb793d
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-07-19 13:44:43 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -14,17 +14,6 @@ async def ping(ctx):
 async def run(ctx,*,cmd):
  run(cmd)
  await ctx.send(sys.stdout)
-#ステータス欄
-@tasks.loop(seconds=40)
-async def loop():
- await bot.change_presence(activity=discord.Game("てすと1"))
- time.sleep(10)
- await bot.change_presence(activity=discord.Game("てすと2"))
- time.sleep(10)
- await bot.change_presence(activity=discord.Game("てすと3"))
- time.sleep(10)
- await bot.change_presence(activity=discord.Game("てすと4"))
- time.sleep(10)
 #機能時のログ
 @bot.event
 async def on_ready():
@@ -33,7 +22,6 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    loop.start()
-    await bot.process_commands()
+    await bot.change_presence(activity=discord.Game("てすと1"))
 
 bot.run('NzE3NTkwOTc2MTU1MjIyMDI4.Xu2_bw.MD4oJ_ZuFNYljkLsJiZi3zxMTy4')
