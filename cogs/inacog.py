468d905004e997d0cff4670d4759db1d8e5c9c93
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-07-23 11:55:33 +0900

    Update inacog.py

--- cogs/inacog.py
+++ cogs/inacog.py
@@ -3,23 +3,18 @@ from discord.ext import commands
 class inacog(commands.Cog):
     @commands.command()
     async def ping(self,ctx):
-     await ctx.send(f'pong! took:{bot.latency}ms')
+     await ctx.send(f'pong! took:{self.bot.latency}ms')
     @commands.command()
-    async def search(self,ctx,*,keyword):
+    async def search(self,ctx,*,key):
+     keyword = key.replace(' ','+')
      await ctx.send(f'https://www.google.com/search?q={keyword}')
     @commands.command()
-    async def what(self,ctx, what):
-     await ctx.send(f'{what}とはなんですか？')
-    @commands.command()
-    async def honyaku(self,ctx,geng1,geng2,*,honyaku):
+    async def honyaku(self,ctx,*,honyaku):
      from googletrans import Translator
      translator = Translator()
-     aaa = translator.translate(honyaku, src=geng1, dest=geng2)
-     await ctx.send(aaa.text)
-    @commands.command()
-    async def vc(self,ctx):
-     channel = ctx.get_channel(ctx.VoiceChannel.id)
-     vc = await channel.connect()
+     detect = translator.detect(honyaku)
+     ans = translator.translate(honyaku.lang, src=detect, dest=jp)
+     await ctx.send(ans.text)
                          
 def setup(bot):
     bot.add_cog(inacog(bot))
