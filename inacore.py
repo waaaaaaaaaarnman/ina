c1b3f1441fa3daa8c676582cd858812930517b20
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-07-25 10:50:55 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -3,17 +3,10 @@
 import discord
 from discord.ext import commands
 import cogs.inacog as inacog
-from discord.ext import tasks
-import time
 import os
 
 bot = commands.Bot(command_prefix='i!')
 
-@bot.command()
-async def test(ctx):
-    await ctx.send(ctx.channel.id)
-
-#機能時のログ
 @bot.event
 async def on_ready():
     print('------')
