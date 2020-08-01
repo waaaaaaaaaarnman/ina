0e91260ce1edc04780e8478be5c098b21ad65166
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-01 09:16:04 +0900

    Update inacog.py

--- cogs/inacog.py
+++ cogs/inacog.py
@@ -12,15 +12,23 @@ class inacog(commands.Cog):
      keyword = key.replace(' ','+')
      await ctx.send(f'https://www.google.com/search?q={keyword}')
     @commands.command()
-    async def honyaku(self,ctx,*,honyaku):
-     from googletrans import Translator
-     translator = Translator()
-     detect = translator.detect(honyaku)
-     ans = translator.translate(honyaku, src=detect, dest=jp)
-     await ctx.send(ans.text)
-
+    async def ank(ctx,main,sub,*,emoji):
+     ankmsgm = discord.Embed(title=main,description=sub)
+     ankmsg = await ctx.send(embed=ankmsgm)
+     ankid.edit(ankmsgm.add_field(name="以下のコマンドで集計:",value=f't!poll {ankmsg.id}))
+     emojisp = emoji.split()
+     for rea in emojisp:
+      await ankmsg.add_reaction(rea)
+    @commands.command()
+    async def poll(ctx,id):
+     poll = await ctx.fetch_message(int(id))
+     reactions = poll.reactions
+     for reaction in reactions:
+      pollans = f'絵文字:{reaction.emoji} カウント:{reaction.count}'
+      await ctx.send(pollans)
+                                  
 def setup(bot):
     bot.add_cog(inacog(bot))
     print('testcog load ok')
 
-    
+   
