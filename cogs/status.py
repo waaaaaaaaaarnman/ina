26fef6ea443f7e02880698d27e6db950a445a82a
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-08 12:18:22 +0900

    Update status.py

--- cogs/status.py
+++ cogs/status.py
@@ -1,4 +1,10 @@
-from discord.ext import commands
-class inacog(commands.Cog):
+from discord.ext import commands,tasks
+class status(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.statusloop.start()
+    @tasks.loop(seconds=40)
+    async def statusloop(self):
+        print('loop ok')
+def setup(bot):
+    bot.add_cog(status(bot))
