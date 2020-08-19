1d93dd54ed3ad038fbd497f766c0e6a1e9fd873a
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-19 12:24:54 +0900

    Update status.py

--- cogs/status.py
+++ cogs/status.py
@@ -3,7 +3,7 @@ import time
 class status(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        statusloop.start()
+        self.bot.statusloop.start()
     @tasks.loop(seconds=40)
     async def statusloop(self):
       await bot.change_presence(activity=discord.Game(f"導入サーバー数:{len(self.bot.guilds)}"))
