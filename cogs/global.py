a88354b70ae444a0fe09d2bcf12c60041431d308
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-19 12:31:03 +0900

    Update global.py

--- cogs/global.py
+++ cogs/global.py
@@ -1,1 +1,24 @@
 
+    @commands.Cog.listener()
+    async def on_message(self, message):
+if message.author.bot:
+        return
+    GLOBAL_CH_NAME = "hoge-global" # グローバルチャットのチャンネル名
+    GLOBAL_WEBHOOK_NAME = "hoge-webhook" # グローバルチャットのWebhook名
+
+    if message.channel.name == GLOBAL_CH_NAME:
+        # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
+        await message.delete()
+
+        channels = client.get_all_channels()
+        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+
+        for channel in global_channels:
+            ch_webhooks = await channel.webhooks()
+            webhook = discord.utils.get(ch_webhooks, name=GLOBAL_WEBHOOK_NAME)
+
+            if webhook is None:
+              channel.create_webhook(name=GLOBAL_WEBHOOK_NAME)
+            await webhook.send(content=message.content,
+                username=message.author.name,
+                avatar_url=message.author.avatar_url_as(format="png"))
