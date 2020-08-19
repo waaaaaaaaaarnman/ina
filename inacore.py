baa95bdf69e3b77281dd10cbfd79bf68a00d98f4
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-19 12:40:13 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -1,25 +1,11 @@
 #coding: utf-8
-
 import discord
 from discord.ext import commands,tasks
 import cogs.inacog as inacog
 import cogs.status as status
+import cogs.global as globa
 import os
-import time
-
 bot = commands.Bot(command_prefix='i!')
-
-@tasks.loop(seconds=40)
-async def loop():
-      await bot.change_presence(activity=discord.Game(f"導入サーバー数:{len(self.bot.guilds)}"))
-      time.sleep(10)
-      await bot.change_presence(activity=discord.Game("てすと"))
-      time.sleep(10)
-      await bot.change_presence(activity=discord.Game("てすと"))
-      time.sleep(10)
-      await bot.change_presence(activity=discord.Game("てすと"))
-      time.sleep(10)          
-  
 @bot.event
 async def on_ready():
     print('------')
@@ -29,5 +15,5 @@ async def on_ready():
     print('------')
     inacog.setup(bot)
     status.setup(bot)
-
+    globa.setup(bot)
 bot.run(os.environ["BOT_TOKEN"])
