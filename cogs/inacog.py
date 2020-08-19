78d40613307d94fdcf42abeef83444ed33174fec
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-19 19:38:13 +0900

    Update inacog.py

--- cogs/inacog.py
+++ cogs/inacog.py
@@ -28,7 +28,7 @@ class inacog(commands.Cog):
         helpembed.add_field(name="search <検索したいワード(スペースOK)>",value="Googleの検索ができます。")
         helpembed.add_field(name="update_notice",value="アップデート情報のチャンネルをフォローできます。")
         helpembed.add_field(name="global_chat",value="グローバルチャットに接続できます。")
-        helpembed.add_field(value="[公式サバはこちら](https://discord.gg/74ZtTMK)")
+        helpembed.add_field(name="公式サバ",value="[公式サバはこちら](https://discord.gg/74ZtTMK)")
         await ctx.send(embed=helpembed)
        
 def setup(bot):
