a97af978fd72d3a7c00ea93d92f2f28f69cdbbaa
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-19 16:27:35 +0900

    Update inacog.py

--- cogs/inacog.py
+++ cogs/inacog.py
@@ -5,7 +5,7 @@ class inacog(commands.Cog):
         self.bot = bot
     @commands.command()
     async def ping(self,ctx):
-     await ctx.send(f'pong! took:{round(self.bot.latency,1)}ms')
+     await ctx.send(f'pong! took:{round(self.bot.latency,2)}ms')
     @commands.command()
     async def search(self,ctx,*,key):
      keyword = key.replace(' ','+')
@@ -14,8 +14,17 @@ class inacog(commands.Cog):
     async def update_notice(self,ctx):
      await self.bot.get_channel(745531061160640572).follow(destination=ctx.channel)
      await ctx.send("アナウンスチャンネルをフォローしたよ！")
-     
-   
+    @commands.command()
+    async def global_chat(self,ctx):
+     if ctx.channel.name == 'ina-global':
+        
+    @command.command()
+    async def help(self,ctx):
+       helpembed = discord.Embed(title="Help",description="コマンドのヘルプだぜぇ") 
+       helpembed.add_field(name="ping",value="Ping値を測定できます。")
+       helpembed.add_field(name="search <検索したいワード(スペースOK)>",value="Googleの検索ができます。")
+       helpembed.add_field(name="update_notice",value="アップデート情報のチャンネルをフォローできます。")
+       helpembed.add_field(name="global_chat",value="グローバルチャットに接続できます。")
 def setup(bot):
     bot.add_cog(inacog(bot))
     print('testcog load ok')
