5df679078760ffc363a9fa9cbe84ff2201e3b47a
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-19 16:12:36 +0900

    Update status.py

--- cogs/status.py
+++ cogs/status.py
@@ -1,17 +1,12 @@
 from discord.ext import commands,tasks
-import time,discord,datetime
+import time,discord
 class status(commands.Cog):
-    @tasks.loop(seconds=40)
+    @tasks.loop(seconds=20)
     async def statusloop(self):
       await self.bot.change_presence(activity=discord.Game(f"導入サーバー数:{len(self.bot.guilds)}"))
       time.sleep(10)
-      dt = datetime.datetime.now()
-      await self.bot.change_presence(activity=discord.Game(f"今の時間は{dt.hour}:{dt.minute}です!"))
-      time.sleep(10)
       await self.bot.change_presence(activity=discord.Game(f"メンバー数:{len(self.bot.users)}"))
       time.sleep(10)
-      await self.bot.change_presence(activity=discord.Game("てすと3"))
-      time.sleep(10)          
     def __init__(self, bot):
         self.bot = bot
         self.statusloop.start()
