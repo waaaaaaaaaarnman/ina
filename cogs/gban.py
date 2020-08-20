45a407f0ef79caa01daa52c6c8a306e8fa2a09eb
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-20 17:51:04 +0900

    Update gban.py

--- cogs/gban.py
+++ cogs/gban.py
@@ -1,8 +1,16 @@
 from discord.ext import commands
 import discord
 class gban(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+ def __init__(self, bot):
+  self.bot = bot
+ @commands.group()
+ async def gban(self,ctx):
+  if ctx.invoked_subcommand is None:
+   await ctx.send('このコマンドにはサブコマンドが必要です。')
+ @gban.command()
+ async def chusei(self,ctx,banorunban,banid,banreason):
+    bandata = await self.bot.get_channel(745805249779990648).fetch_message(745906962197381140)
+    bandata.edit(content=f'{bandata.content
 def setup(bot):
-    bot.add_cog(gban(bot))
+ bot.add_cog(gban(bot))
     print('Gban Loaded!')
