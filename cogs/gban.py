06bdb429cf9c3b7e191049e41f2dd9bb453d13d0
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-20 18:16:10 +0900

    Update gban.py

--- cogs/gban.py
+++ cogs/gban.py
@@ -12,8 +12,17 @@ class gban(commands.Cog):
     await self.bot.get_channel(745918268308717568).send(f'{ctx.author}さんが{banorunban}を申請しています。Id:{banid}理由:{banreason}')
  @gban.command()
  async def gban(self,ctx,banorunban,banid,banreason):
-  bandata = await self.bot.get_channel(745805249779990648).fetch_message(745906962197381140)
-  await bandata.edit(content=f'{bandata.content} {banorunban}:{banid}:{banreason}')
+  if banorunban == 'ban':
+   bandata = await self.bot.get_channel(745805249779990648).fetch_message(745906962197381140)
+   await bandata.edit(content=f'{bandata.content} {banid}:{banreason}')
+   banuser = self.bot.fetch_user(banid)
+   for ban in self.bot.guilds:
+    await ban(banuser,banreason)
+  if banorunban == 'unban':
+   bandata = await self.bot.get_channel(745805249779990648).fetch_message(745906962197381140)
+   await bandata.edit(content=f'{bandata.content.relpace('{banid}:{banreason}','')
+   for unban in self.bot.guilds:
+    await unban(banuser,banreason)
 def setup(bot):
  bot.add_cog(gban(bot))
     print('Gban Loaded!')
