cfec3099291f59a897bafffad6c874c3e913b828
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-20 12:22:51 +0900

    Update globalch.py

--- cogs/globalch.py
+++ cogs/globalch.py
@@ -28,9 +28,7 @@ class globalch(commands.Cog):
       if str(message.channel.id) == globalch:
         await message.delete()
         for channel in global_channels:
-            print(type(channel),channel,global_channels)
             channeldata = self.bot.get_channel(int(channel))
-            print(channeldata)
             ch_webhooks = await channeldata.webhooks()
             webhook = discord.utils.get(ch_webhooks, name=GLOBAL_WEBHOOK_NAME)
             await webhook.send(content=message.content,
