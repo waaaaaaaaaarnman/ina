dce22025952afe271b0a42a04d459f92770abe4b
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-20 10:32:16 +0900

    Update globalch.py

--- cogs/globalch.py
+++ cogs/globalch.py
@@ -8,14 +8,14 @@ class globalch(commands.Cog):
      global_ch = self.bot.get_channel(745805249779990648)
      global_channel = await global_ch.fetch_message(745814673265393794)
      global_channels = global_channel.content.split()
+     print(global_channels)
      GLOBAL_WEBHOOK_NAME = "ina-global-webhook"
+     print(message.channel.id)
      if message.channel.id == global_channels:
         await message.delete()
         for channel in global_channels:
             ch_webhooks = await channel.webhooks()
             webhook = discord.utils.get(ch_webhooks, name=GLOBAL_WEBHOOK_NAME)
-            if webhook is None:
-             await channel.create_webhook(name=GLOBAL_WEBHOOK_NAME)
             await webhook.send(content=message.content,
                                username=message.author.name,
                                avatar_url=message.author.avatar_url_as(format="png"))
