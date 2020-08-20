0a5873cdf5d6e8479c1ed3e6dcd1284f310b5c94
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-20 09:50:03 +0900

    Update globalch.py

--- cogs/globalch.py
+++ cogs/globalch.py
@@ -5,8 +5,7 @@ class globalch(commands.Cog):
         self.bot = bot
     @commands.Cog.listener()
     async def on_message(self, message):
-     with open('datas/global_channel.json','r') as f:
-            global_channels = f.load().split()
+     global_channels = self.bot.fetch_message(745805308378480680).content.split()
      if message.author.bot:
         return
      GLOBAL_WEBHOOK_NAME = "ina-global-webhook"
