4e311c039f30bb25174e16e380327e5715e8ab61
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-21 21:16:36 +0900

    Update gban.py

--- cogs/gban.py
+++ cogs/gban.py
@@ -1,8 +1,11 @@
 from discord.ext import commands
 import discord
-class status(commands.Cog):
+class gban(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    @commands.command()
+    async def gban(self,ctx,banid,banreason):
+        
 def setup(bot):
-    bot.add_cog(status(bot))
-    print(' Loaded!')
+    bot.add_cog(gban(bot))
+    print('Gban Loaded!')
