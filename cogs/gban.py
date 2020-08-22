45cc0a81366ea5836076194551865a698160f881
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-22 11:11:30 +0900

    Update gban.py

--- cogs/gban.py
+++ cogs/gban.py
@@ -6,13 +6,13 @@ class gban(commands.Cog):
     @commands.command()
     async def gban(self,ctx,banid,banreason):
      if str(ctx.author) == 'Kaede_728#8140':
-       gban =  self.bot.fetch_user(int(banid))
+       gban = await self.bot.fetch_user(int(banid))
        await gban.ban(reason=banreason)
        await ctx.send('gbanが完了しました')
     @commands.command()
     async def ungban(self,ctx,ungbanid,ungbanreason):
       if str(ctx.author) == 'Kaede_728#8140':
-       ungban = self.bot.fetch_user(int(banid))
+       ungban = await self.bot.fetch_user(int(banid))
        await ungban.unban(reason=ungbanreason)
        await ctx.send('ungbanが完了しました')
 def setup(bot):
