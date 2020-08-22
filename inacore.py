cf5f2018e5df49b1c38c40772e5bbc5575dee877
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-22 20:40:17 +0900

    Update inacore.py

--- inacore.py
+++ inacore.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 import cogs.inacog as inacog
 import cogs.status as status
 import cogs.globalch as globa
-import cogs.bumptuti as bump
 bot = commands.Bot(command_prefix='i!',help_command=None)
 bot.remove_command("help")
 @bot.event
@@ -21,5 +20,4 @@ async def on_ready():
     inacog.setup(bot)
     status.setup(bot)
     globa.setup(bot)
-    bump.setup(bot)
 bot.run(os.environ["BOT_TOKEN"])
