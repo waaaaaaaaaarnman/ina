4373fed24412106e8a610334e3134e75f97d6999
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-22 11:30:39 +0900

    Update gban.py

--- cogs/gban.py
+++ cogs/gban.py
@@ -6,15 +6,17 @@ class gban(commands.Cog):
     @commands.command()
     async def gban(self,ctx,banid,banreason):
      if str(ctx.author) == 'Kaede_728#8140':
-       gban = await self.bot.fetch_user(int(banid))
+      for guild in self.bot.guilds:  
+       gban = guild.get_member(int(banid))
        await gban.ban(reason=banreason)
        await ctx.send('gbanが完了しました')
     @commands.command()
     async def ungban(self,ctx,ungbanid,ungbanreason):
       if str(ctx.author) == 'Kaede_728#8140':
-       ungban = await self.bot.fetch_user(int(banid))
-       await ungban.unban(reason=ungbanreason)
-       await ctx.send('ungbanが完了しました')
+        for guild in self.bot.guilds:  
+         ungban = guild.get_member(int(ungbanid))
+         await ungban.unban(reason=ungbanreason)
+         await ctx.send('ungbanが完了しました')
 def setup(bot):
     bot.add_cog(gban(bot))
     print('Gban Loaded!')
