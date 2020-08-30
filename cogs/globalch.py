b5804ade61c46ef48c5f84996d0703a864a8d8d3
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-30 17:49:56 +0900

    Update globalch.py

--- cogs/globalch.py
+++ cogs/globalch.py
@@ -43,6 +43,8 @@ class globalch(commands.Cog):
     async def on_message(self, message):
      if message.author.bot:
         return
+     if message.content.startwith('i!'):
+        return
      global_ch = self.bot.get_channel(745805249779990648)
      global_channel = await global_ch.fetch_message(745814673265393794)
      global_channels = global_channel.content.split()
