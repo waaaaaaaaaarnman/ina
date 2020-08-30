57c471b60f4958541448c0e70c73e20454a9668d
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-30 20:20:49 +0900

    Update inacog.py

--- cogs/inacog.py
+++ cogs/inacog.py
@@ -1,5 +1,27 @@
 from discord.ext import commands
 import discord,random,libneko
+import asyncio
+import io
+import traceback
+import textwrap
+import contextlib
+import os
+def cleanup_code(content):
+    if content.startswith('```') and content.endswith('```'):
+        return '\n'.join(content.split('\n')[1:-1])
+    return content.strip('` \n')
+
+def get_syntax_error(e):
+    if e.text is None:
+        return f'```py\n{e.__class__.__name__}: {e}\n```'
+    return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
+
+def mention_to_user_id(mention):
+    user_id = mention.strip("<@").strip(">")
+    if user_id.find("!") != -1:
+        user_id = user_id.strip("!")
+    return int(user_id)
+
 def default_buttons():
     from libneko.pag.reactionbuttons import (
         first_page,
@@ -68,6 +90,45 @@ class inacog(commands.Cog):
     async def omikuji(self,ctx):
      omikujis = ['大凶','凶','吉','小吉','中吉','大吉']
      await ctx.send(random.choice(omikujis))
+    @commands.command()
+    async def eval(self, ctx):
+        try:
+           if str(ctx.author)  == 'Kaede_728#8140':
+
+            env = {'bot': self.bot, 'ctx': ctx, 'channel': ctx.channel, 'author': ctx.author, 'guild': ctx.guild,
+                   'message': ctx.message, '_': self._last_result}
+            env.update(globals())
+            body = cleanup_code(ctx.message.content[6:].lstrip())
+            stdout = io.StringIO()
+            to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+            try:
+                exec(to_compile, env)
+            except Exception as e:
+                return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+            func = env['func']
+            try:
+                with contextlib.redirect_stdout(stdout):
+                    ret = await func()
+            except Exception as _:
+                value = stdout.getvalue()
+                await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+            else:
+                value = stdout.getvalue()
+                try:
+                    await ctx.message.add_reaction('\u2705')
+                except Exception:
+                    pass
+                if ret is None:
+                    if value:
+                        await ctx.send(f'```py\n{value}\n```')
+                else:
+                    self._last_result = ret
+                    await ctx.send(f'```py\n{value}{ret}\n```')
+
+        except (NotFound, asyncio.TimeoutError, Forbidden):
+            return
+        except:
+            return print("エラー情報\n" + traceback.format_exc())
 def setup(bot):
     bot.add_cog(inacog(bot))
     print('TestCog Loaded!')
