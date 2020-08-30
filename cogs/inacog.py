b0c822bff5e75a117dbdf0ed4082eb32007200ed
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-30 20:08:02 +0900

    Update inacog.py

--- cogs/inacog.py
+++ cogs/inacog.py
@@ -1,5 +1,24 @@
 from discord.ext import commands
-import discord,random
+import discord,random,libneko
+def default_buttons():
+    from libneko.pag.reactionbuttons import (
+        first_page,
+        back_10_pages,
+        previous_page,
+        next_page,
+        forward_10_pages,
+        last_page
+    )
+
+    return (
+        first_page(),
+        back_10_pages(),
+        previous_page(),
+        next_page(),
+        forward_10_pages(),
+        last_page()
+    )
+buttons = default_buttons()
 class inacog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -23,18 +42,24 @@ class inacog(commands.Cog):
         about.add_field(name="サーバー数",value=len(self.bot.guilds))
         await ctx.send(embed=about)
     @commands.command()
-    async def help(self,ctx):
-        helpembed = discord.Embed(title="Help",description="コマンドのヘルプだぜぇ",color=discord.Colour.from_rgb(46,222,16))
-        helpembed.add_field(name="ping",value="Ping値を測定できます。")
-        helpembed.add_field(name="search <検索したいワード(スペースOK)>",value="Googleの検索ができます。")
-        helpembed.add_field(name="update_notice",value="アップデート情報のチャンネルをフォローできます。")
-        helpembed.add_field(name="global_chat_<on,off>",value="グローバルチャットに接続できます。")
-        helpembed.add_field(name="bugandidea <文章>",value="バグ報告とアイデア送信ができます。")
-        helpembed.add_field(name="omikuji",value="おみぐじができます。")
-        helpembed.add_field(name="about",value="ボットの情報が表示されます。")
-        helpembed.add_field(name="公式サバ",value="[公式サバはこちら](https://discord.gg/tZqkn8H)")
-        helpembed.add_field(name="公式サイト",value="[公式サイトはこちら](https://waaaaaaaaaarnman.github.io/ina/top/)")
-        await ctx.send(embed=helpembed)
+    async def help(self, ctx):
+            pages = [(discord.Embed(title="ヘルプコマンド")),
+                     (discord.Embed(title="お遊びコマンド")),
+                     (discord.Embed(title="グローバルチャットのコマンド")),
+                     (discord.Embed(title="その他コマンド"))]
+            pages[0].add_field(name="i!about", value="このBotについてのInfoを表示します。")
+            pages[0].add_field(name='i!help', value='このヘルプを表示します。')
+            pages[0].add_field(name="公式サバ",value="[公式サバはこちら](https://discord.gg/tZqkn8H)")
+            pages[0].add_field(name="公式サイト",value="[公式サイトはこちら](https://waaaaaaaaaarnman.github.io/ina/top/)")
+            pages[1].add_field(name="i!omjkuji", value="おみくじができます。~~ですがかなり地味です。~~")
+            pages[2].add_field(name="i!global＿chat＿<on or off>", value="グローバルチャットをオンオフできます。")
+            pages[3].add_field(name="i!search <調べたい単語>", value="グローバルチャットに登録します。")
+            pages[3].add_field(name="i!ping", value="Ping測定ができます。")
+            pages[3].add_field(name="i!bugandidea", value="レポートができます。")
+            nav = libneko.pag.navigator.EmbedNavigator(ctx, pages, buttons=default_buttons(), timeout=30)
+            nav.start()
+            await ctx.send(nav)
+    
     @commands.command()
     async def bugandidea(self,ctx,bugandidea):
      await self.bot.get_channel(745601986669576192).send(f'{ctx.author}さん:{bugandidea}')
