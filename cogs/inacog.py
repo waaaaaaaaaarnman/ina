1e1c54541f65952c2ca0e8bdf912a601ba1baa3b
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-08-30 20:13:51 +0900

    Update inacog.py

--- cogs/inacog.py
+++ cogs/inacog.py
@@ -53,7 +53,7 @@ class inacog(commands.Cog):
             pages[0].add_field(name="公式サイト",value="[公式サイトはこちら](https://waaaaaaaaaarnman.github.io/ina/top/)")
             pages[1].add_field(name="i!omjkuji", value="おみくじができます。~~ですがかなり地味です。~~")
             pages[2].add_field(name="i!global＿chat＿<on or off>", value="グローバルチャットをオンオフできます。")
-            pages[3].add_field(name="i!search <調べたい単語>", value="グローバルチャットに登録します。")
+            pages[3].add_field(name="i!search <調べたい単語>", value="Googleで検索ができます。")
             pages[3].add_field(name="i!ping", value="Ping測定ができます。")
             pages[3].add_field(name="i!bugandidea", value="レポートができます。")
             nav = libneko.pag.navigator.EmbedNavigator(ctx, pages, buttons=default_buttons(), timeout=30)
