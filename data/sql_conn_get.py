9b00a05ba098eec726f1898f13cba8191cfe8546
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-09-06 12:40:40 +0900

    Update sql_conn_get.py

--- data/sql_conn_get.py
+++ data/sql_conn_get.py
@@ -7,7 +7,7 @@ user = dburl.user
 database = dburl.path[1:]
 port = dburl.port
 password = dburl.password
-async def get_conn():
+def get_conn():
     conn = await asyncpg.connect(
         host = host ,
         user = user, 
