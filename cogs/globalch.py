f116903425801d54ce76aab93e54cee9e306f965
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-09-06 13:35:02 +0900

    Update globalch.py

--- cogs/globalch.py
+++ cogs/globalch.py
@@ -1,6 +1,28 @@
 from discord.ext import commands
 import discord,asyncio
-import ru as con
+import os
+import asyncpg
+from yarl import URL
+dburl = URL(os.environ["DATABASE_URL"])
+host = dburl.host
+user = dburl.user
+database = dburl.path[1:]
+port = dburl.port
+password = dburl.password
+async def DB(SQL):
+    #printするときに使ったやつをついでにURLをそのままでもよし
+    conn = await asyncpg.connect(
+        host = host ,
+        user = user, 
+        database = database, 
+        port = port, 
+        password = password
+        )
+    #SQLを実行
+    values = await conn.fetch(SQL)
+    #接続を切る
+    await conn.close()
+    return values
 class globalch(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
