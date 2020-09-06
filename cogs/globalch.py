70835f0e09d1937010c1b0a25467a961f3eb1946
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-09-06 17:42:35 +0900

    Update globalch.py

--- cogs/globalch.py
+++ cogs/globalch.py
@@ -66,7 +66,7 @@ class globalch(commands.Cog):
      for data in await DB('select * from globalch'):
       globalch = str(data['id'])
       if str(message.channel.id) == globalch:
-        for channell in await DB('select * from globalch']:
+        for channell in await DB('select * from globalch'):
             channel = chanell['id']
             if str(message.channel.id) == channel:
              break
